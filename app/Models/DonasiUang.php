b317b8731efde91d6ac127b86dab7f7f2af09126
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-03-25 06:42:49 +0700

    Create DonasiUang.php

---
+++ app/Models/DonasiUang.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DonasiUang extends Model
+{
+    use HasFactory;
+}
