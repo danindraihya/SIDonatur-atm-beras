30b058abf3a63100d6ad5db674d9e35fddb44ca0
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-03-25 09:21:09 +0700

    update donasi-beras model

--- app/Models/DonasiBeras.php
+++ app/Models/DonasiBeras.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class DonasiBeras extends Model
 {
     use HasFactory;
+
+    protected $table = 'donasi_beras';
+    
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $fillable = [
+        'id',
+        'donatur_id',
+        'nominal',
+    ];
+
+    public function donatur()
+    {
+        return $this->belongsTo(Donatur::class, 'donatur_id', 'id');
+    }
 }
