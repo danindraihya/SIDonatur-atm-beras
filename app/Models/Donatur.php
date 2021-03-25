70b25affbe4aed39581aa9c669205f8ba989755c
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-03-25 09:21:21 +0700

    update donatur model

--- app/Models/Donatur.php
+++ app/Models/Donatur.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Donatur extends Model
 {
     use HasFactory;
+
+    protected $table = 'donatur';
+    
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $fillable = [
+        'id',
+        'jenis_donatur_id',
+        'nama',
+        'alamat',
+        'nomor_ponsel',
+    ];
+
+    public function jenisDonatur()
+    {
+        return $this->belongsTo(JenisDonatur::class, 'jenis_donatur_id', 'id');
+    }
+
+    public function listDonasiUang()
+    {
+        return $this->hasMany(DonasiUang::class, 'donatur_id', 'id');
+    }
+
+    public function listDonasiBeras()
+    {
+        return $this->hasMany(DonasiBeras::class, 'donatur_id', 'id');
+    }
 }
