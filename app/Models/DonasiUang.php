4b9cde12ddcbeaea5e9ed6fd1b999d0de2cd722c
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-03-25 09:21:15 +0700

    update donasi-uang model

--- app/Models/DonasiUang.php
+++ app/Models/DonasiUang.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class DonasiUang extends Model
 {
     use HasFactory;
+
+    protected $table = 'donasi_uang';
+    
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $fillable = [
+        'id',
+        'donatur_id',
+        'jumlah',
+    ];
+
+    public function donatur()
+    {
+        return $this->belongsTo(Donatur::class, 'donatur_id', 'id');
+    }
 }
