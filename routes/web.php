c7a1dfe0fa7f0ea1415dc1aab43f4f5bddba1c24
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-04-01 09:20:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JenisDonaturController;
 use App\Http\Controllers\DonaturController;
 
 /*
@@ -14,21 +16,18 @@ use App\Http\Controllers\DonaturController;
 |
 */
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::prefix('jenis-donatur')->group(function () {
-
+    Route::get('/', [JenisDonaturController::class, 'index'])->name('jenis_donatur.index');
+    Route::post('/store', [JenisDonaturController::class, 'store'])->name('jenis_donatur.store');
+    Route::put('/update', [JenisDonaturController::class, 'update'])->name('jenis_donatur.update');
+    Route::delete('/destroy', [JenisDonaturController::class, 'destroy'])->name('jenis_donatur.destroy');
 });
-
 Route::prefix('donatur')->group(function () {
     Route::get('/', [DonaturController::class, 'index'])->name('donatur.index');
     Route::post('/store', [DonaturController::class, 'store'])->name('donatur.store');
     Route::put('/update', [DonaturController::class, 'update'])->name('donatur.update');
     Route::delete('/destroy', [DonaturController::class, 'destroy'])->name('donatur.destroy');
 });
-
-Route::prefix('donasi-uang')->group(function () {
-
-});
-
-Route::prefix('donasi-beras')->group(function () {
-
-});
+Route::prefix('donasi-uang')->group(function () {});
+Route::prefix('donasi-beras')->group(function () {});
