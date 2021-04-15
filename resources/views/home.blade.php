eebb50855770715d375c0603036ffd7c1e3da69f
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-04-15 14:03:59 +0700

    update dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,49 @@
 @extends('layouts.base')
 
 @section('title', 'Dasbor')
+
+@section('content')
+<div class="row">
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-info elevation-1">
+                <i class="fas fa-users"></i>
+            </span>
+            <div class="info-box-content">
+                <span class="info-box-text">Jumlah Donatur</span>
+                <span class="info-box-number">
+                    10
+                    <small>Orang</small>
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-success elevation-1">
+                <i class="fas fa-wallet"></i>
+            </span>
+            <div class="info-box-content">
+                <span class="info-box-text">Jumlah Donasi Uang Terkumpul</span>
+                <span class="info-box-number">
+                    @rupiah(150000000000)
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-warning elevation-1">
+                <i class="fas fa-utensils"></i>
+            </span>
+            <div class="info-box-content">
+                <span class="info-box-text">Jumlah Donasi Beras Terkumpul</span>
+                <span class="info-box-number">
+                    @number(250)
+                    <small>Kilogram</small>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
