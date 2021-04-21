8bc24df171b0dad26f2d62807166543e6580121b
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-04-21 12:25:06 +0700

    update dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Donatur;
 use App\Models\DonasiUang;
 use App\Models\DonasiBeras;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
+    private $chartLabel;
+    private $chartDataDonasiUang;
+    private $chartDataDonasiBeras;
+
+    public function __construct()
+    {
+        $this->chartLabel = collect();
+        $this->chartDataDonasiUang = collect();
+        $this->chartDataDonasiBeras = collect();
+    }
+
     public function index()
     {
+        $this->generateChartData();
         $data = [
             'jumlahDonatur' => Donatur::all()->count(),
-            'donasiUang' => DonasiUang::all()->sum('nominal'),
-            'donasiBeras' => DonasiBeras::all()->sum('jumlah'),
+            'jumlahDonasiUang' => DonasiUang::all()->sum('nominal'),
+            'jumlahDonasiBeras' => DonasiBeras::all()->sum('jumlah'),
+            'chartLabel' => $this->chartLabel,
+            'chartDataDonasiUang' => $this->chartDataDonasiUang,
+            'chartDataDonasiBeras' => $this->chartDataDonasiBeras,
         ];
         return view('home', $data);
+    }
+
+    private function generateChartData()
+    {
+        for ($i = 30; $i >= 0; $i--) {
+            $date = Carbon::today()->subDay($i);
+            $this->chartLabel->push($date->format('d/m'));
+            $this->chartDataDonasiUang->push($this->sumDonasiUangByDate($date));
+            $this->chartDataDonasiBeras->push($this->sumDonasiBerasByDate($date));
+        }
+    }
+
+    private function sumDonasiUangByDate($date)
+    {
+        return DonasiUang::query()
+            ->whereDate('created_at', $date)
+            ->sum('nominal');
+    }
+
+    private function sumDonasiBerasByDate($date)
+    {
+        return DonasiBeras::query()
+            ->whereDate('created_at', $date)
+            ->sum('jumlah');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,47 +3,102 @@
 @section('title', 'Dasbor')
 
 @section('content')
-<div class="row">
-    <div class="col-md-4">
-        <div class="info-box">
-            <span class="info-box-icon bg-info elevation-1">
-                <i class="fas fa-users"></i>
-            </span>
-            <div class="info-box-content">
-                <span class="info-box-text">Jumlah Donatur</span>
-                <span class="info-box-number">
-                    {{ $jumlahDonatur }}
-                    <small>Orang</small>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-info elevation-1">
+                    <i class="fas fa-users"></i>
                 </span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Jumlah Donatur</span>
+                    <span class="info-box-number">
+                        {{ $jumlahDonatur }}
+                        <small>Orang</small>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-success elevation-1">
+                    <i class="fas fa-wallet"></i>
+                </span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Jumlah Donasi Uang Terkumpul</span>
+                    <span class="info-box-number">
+                        @rupiah($jumlahDonasiUang)
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-warning elevation-1">
+                    <i class="fas fa-utensils"></i>
+                </span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Jumlah Donasi Beras Terkumpul</span>
+                    <span class="info-box-number">
+                        @number($jumlahDonasiBeras)
+                        <small>Kilogram</small>
+                    </span>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="info-box">
-            <span class="info-box-icon bg-success elevation-1">
-                <i class="fas fa-wallet"></i>
-            </span>
-            <div class="info-box-content">
-                <span class="info-box-text">Jumlah Donasi Uang Terkumpul</span>
-                <span class="info-box-number">
-                    @rupiah($donasiUang)
-                </span>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Donasi Uang 30 Hari Terakhir</div>
+                <div class="card-body">
+                    <canvas id="chart-donasi-uang"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Donasi Beras 30 Hari Terakhir</div>
+                <div class="card-body">
+                    <canvas id="chart-donasi-beras"></canvas>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="info-box">
-            <span class="info-box-icon bg-warning elevation-1">
-                <i class="fas fa-utensils"></i>
-            </span>
-            <div class="info-box-content">
-                <span class="info-box-text">Jumlah Donasi Beras Terkumpul</span>
-                <span class="info-box-number">
-                    @number($donasiBeras)
-                    <small>Kilogram</small>
-                </span>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
+
+@push('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.1/chart.min.js" integrity="sha512-BqNYFBAzGfZDnIWSAEGZSD/QFKeVxms2dIBPfw11gZubWwKUjEgmFUtUls8vZ6xTRZN/jaXGHD/ZaxD9+fDo0A==" crossorigin="anonymous"></script>
+    <script>
+        const chartDonasiUang = document.getElementById('chart-donasi-uang');
+        const chartDonasiBeras = document.getElementById('chart-donasi-beras');
+        const labels = {!! $chartLabel !!};
+
+        new Chart(chartDonasiUang, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Nominal',
+                        data: {!! $chartDataDonasiUang !!},
+                        fill: false
+                    }
+                ],
+            }
+        });
+
+        new Chart(chartDonasiBeras, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Jumlah',
+                        data: {!! $chartDataDonasiBeras !!},
+                        fill: false
+                    }
+                ],
+            }
+        });
+    </script>
+@endpush
