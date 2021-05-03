bde8230f85f4841019c7b3e59f57a799dc85f3d8
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2021-05-03 22:00:24 +0700

    add middleware grouping

--- routes/web.php
+++ routes/web.php
@@ -17,35 +17,36 @@ use App\Http\Controllers\DonasiBerasController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::prefix('jenis-donatur')->group(function () {
-    Route::get('/', [JenisDonaturController::class, 'index'])->name('jenis_donatur.index');
-    Route::post('/store', [JenisDonaturController::class, 'store'])->name('jenis_donatur.store');
-    Route::put('/update', [JenisDonaturController::class, 'update'])->name('jenis_donatur.update');
-    Route::delete('/destroy', [JenisDonaturController::class, 'destroy'])->name('jenis_donatur.destroy');
-});
-Route::prefix('donatur')->group(function () {
-    Route::get('/', [DonaturController::class, 'index'])->name('donatur.index');
-    Route::post('/store', [DonaturController::class, 'store'])->name('donatur.store');
-    Route::put('/update', [DonaturController::class, 'update'])->name('donatur.update');
-    Route::delete('/destroy', [DonaturController::class, 'destroy'])->name('donatur.destroy');
-});
-Route::prefix('donasi-uang')->group(function () {
-    Route::get('/', [DonasiUangController::class, 'index'])->name('donasi_uang.index');
-    Route::post('/store', [DonasiUangController::class, 'store'])->name('donasi_uang.store');
-    Route::put('/update', [DonasiUangController::class, 'update'])->name('donasi_uang.update');
-    Route::delete('/destroy', [DonasiUangController::class, 'destroy'])->name('donasi_uang.destroy');
-});
-Route::prefix('donasi-beras')->group(function () {
-    Route::get('/', [DonasiBerasController::class, 'index'])->name('donasi_beras.index');
-    Route::post('/store', [DonasiBerasController::class, 'store'])->name('donasi_beras.store');
-    Route::put('/update', [DonasiBerasController::class, 'update'])->name('donasi_beras.update');
-    Route::delete('/destroy', [DonasiBerasController::class, 'destroy'])->name('donasi_beras.destroy');
-});
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    
+    Route::prefix('jenis-donatur')->group(function () {
+        Route::get('/', [JenisDonaturController::class, 'index'])->name('jenis_donatur.index');
+        Route::post('/store', [JenisDonaturController::class, 'store'])->name('jenis_donatur.store');
+        Route::put('/update', [JenisDonaturController::class, 'update'])->name('jenis_donatur.update');
+        Route::delete('/destroy', [JenisDonaturController::class, 'destroy'])->name('jenis_donatur.destroy');
+    });
+    
+    Route::prefix('donatur')->group(function () {
+        Route::get('/', [DonaturController::class, 'index'])->name('donatur.index');
+        Route::post('/store', [DonaturController::class, 'store'])->name('donatur.store');
+        Route::put('/update', [DonaturController::class, 'update'])->name('donatur.update');
+        Route::delete('/destroy', [DonaturController::class, 'destroy'])->name('donatur.destroy');
+    });
+    
+    Route::prefix('donasi-uang')->group(function () {
+        Route::get('/', [DonasiUangController::class, 'index'])->name('donasi_uang.index');
+        Route::post('/store', [DonasiUangController::class, 'store'])->name('donasi_uang.store');
+        Route::put('/update', [DonasiUangController::class, 'update'])->name('donasi_uang.update');
+        Route::delete('/destroy', [DonasiUangController::class, 'destroy'])->name('donasi_uang.destroy');
+    });
+    
+    Route::prefix('donasi-beras')->group(function () {
+        Route::get('/', [DonasiBerasController::class, 'index'])->name('donasi_beras.index');
+        Route::post('/store', [DonasiBerasController::class, 'store'])->name('donasi_beras.store');
+        Route::put('/update', [DonasiBerasController::class, 'update'])->name('donasi_beras.update');
+        Route::delete('/destroy', [DonasiBerasController::class, 'destroy'])->name('donasi_beras.destroy');
+    });
+});
